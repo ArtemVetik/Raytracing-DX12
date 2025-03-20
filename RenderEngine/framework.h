27af50965a2a106e7822490f2938b1e801ca017e
Artem Vetik <av.unitygames@gmail.com>
2025-03-21 00:39:16 +0300

    Import and link assimp and WinPixEvent libs

---
+++ RenderEngine/framework.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+#include <d3d12.h>
+#include "WinPixEventRuntime/pix3.h"
+
+#ifdef RENDERENGINE_EXPORTS
+#define RENDERENGINE_API __declspec(dllexport)
+#else
+#define RENDERENGINE_API __declspec(dllimport)
+#endif
