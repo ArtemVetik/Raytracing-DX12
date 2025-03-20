bffd4d54dcabb4020021ae95321c26046949a719
Artem Vetik <av.unitygames@gmail.com>
2025-03-21 00:30:32 +0300

    Setup windows subsystem

--- Raytracing-DX12/Raytracing-DX12.cpp
+++ Raytracing-DX12/Raytracing-DX12.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <windows.h>
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
 {
-    std::cout << "Hello World!\n";
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	FILE* fp;
+
+	AllocConsole();
+	freopen_s(&fp, "CONIN$", "r", stdin);
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+	freopen_s(&fp, "CONOUT$", "w", stderr);
+#endif
+
+	OutputDebugStringW(L"Hello World!\n");
 }
